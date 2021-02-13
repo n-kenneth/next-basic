a04f7e985272b7da0e9e7c430a8fa9cfaf3d0bcb
Niel Kenneth <nielkenneth@moustacherepublic.com>
2021-02-13 15:29:33 +0800

    next js basics

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import headerStyles from '../styles/Header.module.css';
+
+
+const Header = () => {
+  return (
+    <div className={headerStyles.title}>
+      <h1><span>WebDev</span> News</h1>
+      <p className={headerStyles.description}>Keep up to date with latest news</p>
+    </div>
+  )
+}
+
+export default Header;
